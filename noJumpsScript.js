0390c0d246d16032e2456dc2314c831b0e2049f2
Adrian Polo <adrianfpg14@gmail.com>
2022-06-18 23:43:36 -0500

    New way

--- noJumpsScript.js
+++ noJumpsScript.js
@@ -1,30 +1,62 @@
+//-------------OLD CODE
+// ``const n = document.getElementById('n');
+
+// let a = prompt('Input: ');
+
+// console.log('For:');
+// let gotted;
+// let lineas = [];
+// let linea = '';
+// console.log(a.length);
+
+// for(let i=0; i<a.length; i++){
+//     gotted=a.substring(i,i+1);
+//     if(gotted=='\n' || gotted=='\r'){
+//         if(linea.length>0){
+//             lineas.push(linea);
+//             linea = '';
+//         }
+//     }else{
+//         linea += gotted;
+//         if(i==a.length-1){
+//             lineas.push(linea);
+//         }
+//     }
+// }
+
+// for(let line of lineas){
+//     n.innerHTML += `<p>${line.trim()}</p>`;
+//     console.log(line.trim());
+// }
+
+
+//--------------NEW CODE
+
 const n = document.getElementById('n');
+let input = prompt('Input: ');
+let arr = [], arr2 = [];
 
-let a = prompt('Input: ');
 
-console.log('For:');
-let gotted;
-let lineas = [];
-let linea = '';
-console.log(a.length);
+for(let i=0; i<input.length; i++){
+    arr.push(input.substring(i,i+1));
+}
 
-for(let i=0; i<a.length; i++){
-    gotted=a.substring(i,i+1);
-    if(gotted=='\n' || gotted=='\r'){
-        if(linea.length>0){
-            lineas.push(linea);
-            linea = '';
+let line = '';
+for(let key of arr){
+    if((key == '\n' || key == '\r')){
+        if(line != ''){
+            arr2.push(line.trim());
+            line = '';
         }
     }else{
-        linea += gotted;
-        if(i==a.length-1){
-            lineas.push(linea);
+        if(!((key == ' ') && (line.substring(line.length-1,line.length)) == ' ')){
+            line += key;
         }
     }
 }
+arr2.push(line.trim());
+console.log(arr2);
 
-for(let line of lineas){
-    n.innerHTML += `<p>${line.trim()}</p>`;
-    console.log(line.trim());
+for(let key of arr2){
+    n.innerHTML += `<p>${key}</p>`;
 }
-
